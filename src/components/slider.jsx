06cc0c61b761170114915d399d285e38bb899760
usamashaukt <usamashaukat36@gmail.com>
2024-09-22 13:19:42 +0500

    started added styling

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -2,30 +2,27 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-function Sliderdata( {data}) {
-
+import "./slider.css";
+function Sliderdata({ data }) {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    autoplay: true,
-    slidesToShow: 3,
+    autoplay: false,
+    slidesToShow: 6,
     slidesToScroll: 1,
     arrows: true,
   };
- 
+
   return (
-    
-      
-           <Slider {...settings}>
-      {data.map((content)=><div style={{display:"flex",flexDirection:"row",gap:"10px"}}>
-        <img src={content.image} width={200} height={100} alt="" />
-        <p>{content.title}</p>
-      
-      </div>)}
+    <Slider {...settings}>
+      {data.map((content) => (
+        <div className="slider-container flex flex-row flex-wrap items-center gap-2 " >
+          <img src={content.image} width={200} className="img-slider max-h-12 " alt="" />
+          <p>{content.title}</p>
+        </div>
+      ))}
     </Slider>
-    
-  
   );
 }
 
