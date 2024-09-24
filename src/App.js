7e394391a5709a4b640b44f3d1d0c666a64bb1c9
usmanahmed <nousmanahmed19@gmail.com>
2024-09-25 01:54:25 +0500

    Card

--- src/App.js
+++ src/App.js
@@ -1,55 +1,95 @@
-import logo from "./logo.svg";
-// import "./App.css";
-import Sliderdata from "./components/slider";
 import Card from "./components/card";
+import ImageCard from "./components/image-card";
+import logo from "./logo.svg";
+import image1 from "../src/slider/image1.jpg";
+import ukflag from "../src/slider/uk-flag.png";
+import svgleft from "../src/slider/svgviewer-output (1).png"
+import svgright from "../src/slider/svgviewer-output (2).png"
+
+import "./App.css";
 
 function App() {
-  const sliderData = [
-    {
-      image: "./slider/image1.jpg",
-      title: "Hello",
-    },
-    {
-      image: "./slider/image2.jpg",
-      title: "Hello",
-    },
-    {
-      image: "./slider/image3.jpg",
-      title: "Hello",
-    },
-    {
-      image: "./slider/image4.jpg",
-      title: "Hello",
-    },
-    {
-      image: "./slider/image4.jpg",
-      title: "Hello",
-    },
-    {
-      image: "./slider/image4.jpg",
-      title: "Hello",
-    },
-    {
-      image: "./slider/image4.jpg",
-      title: "Hello",
-    },
-    {
-      image: "./slider/image4.jpg",
-      title: "Hello",
-    },
-    {
-      image: "./slider/image4.jpg",
-      title: "Hello",
-    },
-    {
-      image: "./slider/image4.jpg",
-      title: "Hello",
-    },
-  ];
+  const sliderData = [];
   return (
     <>
-      <Sliderdata data={sliderData} />
-      <Card />
+      <div className="main-card" >
+        <div className="svg-left">
+        <img  src={svgleft} />
+        </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bbbbbbb" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+          <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+       <div>
+       <Card text="hello bro" image={image1} />
+       </div>
+
+       <div className="svgright">
+       <img src={svgright} />  
+        </div>
+       
+      </div>
+      
+      <ImageCard image1={image1} image2={ukflag}  text="hello bro" paragraph="4k best welpaper" />
     </>
   );
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,44 +1,21 @@
-import React from "react";
+import React from 'react'
+import './card.css'
 
-function Card() {
+const Card = ({image, text}) => {
   return (
-    <>
-      <div className="card max-h-[168px] max-w-[299px]">
-        <div>
-          <a href="">
-            <div
-              style={{
-                borderRadius: "12px",
-                overflow: "hidden",
-                width: "100%",
-              }}
-            >
-              <img src="./slider/image1.jpg" alt="" />
-            </div>
-          </a>
+    <div>
+        <div className="Card">
+        <div className="card-image">
+            <img src={image} alt="" />
         </div>
-        <div className="card-footer-text">
-          <a href="">
-            {" "}
-            <div className="footer-logo-text flex flex-row justify-start items-center gap-2">
-              <img
-                className="max-h-[12px]"
-                src="./uk-flag.png"
-                width={12}
-                alt=""
-              />{" "}
-              <h4 className="text-[18px]  font-normal tracking-normal footer text-heading hover:underline ">
-                tooo
-              </h4>
-            </div>
-            <p className="text-[20px]  leading-8 tracking-normal hover:underline ">
-              best 4k wallpaper
-            </p>
-          </a>
+        <div className="card-textbox">
+            <h1>{text}</h1>
         </div>
-      </div>
-    </>
-  );
+        </div>
+        
+        
+    </div>
+  )
 }
 
-export default Card;
+export default Card
