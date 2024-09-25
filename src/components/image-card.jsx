1bf01b79d2c55dda3603d51a61a9c0b9b99aeca5
usamashaukt <usamashaukat36@gmail.com>
2024-09-25 20:30:19 +0500

    added width adjustments

--- src/components/image-card.jsx
+++ src/components/image-card.jsx
@@ -1,27 +1,22 @@
 import React from "react";
 import "./image-card.css";
 
-
-function ImageCard({image1, image2, text, paragraph}) {
+function ImageCard({ image1, image2, text, paragraph }) {
   return (
-    <>
-      <a className="image-item">
-        <div className="img">
-          <img src={image1} alt="" />
-        </div>
-        <div className="content">
+    <a className="image-item">
+      <div className="img">
+        <img src={image1} alt="Main visual" />
+      </div>
+      <div className="content">
         <div className="description">
           <div className="icon">
-          <img src={image2}/>
+            <img src={image2} alt="Icon" />
           </div>
-          
           <p>{paragraph}</p>
         </div>
         <h2>{text}</h2>
-        </div>
-        
-      </a>
-    </>
+      </div>
+    </a>
   );
 }
 
